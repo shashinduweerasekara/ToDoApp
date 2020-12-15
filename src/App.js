15266407378895d790e1b31b834e413b3a877037
shashinduweerasekara <lakshan.shashindu@gmail.com>
2020-12-15 05:41:04 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+import Home from "./components/Home";
+import "./bootstrap.css";
+
+function App() {
+    return (
+        <div className="App row">
+            <Home></Home>
+        </div>
+    );
+}
+
+export default App;
